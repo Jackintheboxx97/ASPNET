a547dc0c8d4e7fc8d5511f388ea0be350ab14945
Jack in the Boxx <kdijahsweets@gmail.com>
2020-02-25 10:28:00 -0600

    Created a Create Product Option for website

--- ASPNET/Controllers/ProductController.cs
+++ ASPNET/Controllers/ProductController.cs
@@ -54,6 +54,21 @@ namespace ASPNET.Controllers
 
             return RedirectToAction("ViewProduct", new { id = product.ProductID });
         }
+        public IActionResult InsertProduct()
+        {
+            var prod = repo.AssignCategory();
+
+            return View(prod);
+        }
+        public IActionResult InsertProductToDatabase(Product productToInsert)
+        {
+            repo.InsertProduct(productToInsert);
+
+            return RedirectToAction("Index");
+        }
+
+
+
 
 
 
