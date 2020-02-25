fbe7d749006dc6169b0db9ee82ec372577946fe7
Jack in the Boxx <kdijahsweets@gmail.com>
2020-02-25 10:52:18 -0600

    Added a delete button

--- ASPNET/Controllers/ProductController.cs
+++ ASPNET/Controllers/ProductController.cs
@@ -66,7 +66,12 @@ namespace ASPNET.Controllers
 
             return RedirectToAction("Index");
         }
+        public IActionResult DeleteProduct(Product product)
+        {
+            repo.DeleteProduct(product);
+            return RedirectToAction("Index");
 
+        }
 
 
 
